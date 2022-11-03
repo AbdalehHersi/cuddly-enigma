663bfcb575547cf5235785c189b40015fe2ba46f
Abdaleh Hersi <therealhersi03@gmail.com>
2022-11-03 12:20:21 +0000

    added router imports and fixed import issues.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtsRoutes = require('./thoughtsRoutes.js');
 const userRoutes = require('./userRoutes');
 
-router.use('/posts', thoughtsRoutes);
+router.use('/thoughts', thoughtsRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
